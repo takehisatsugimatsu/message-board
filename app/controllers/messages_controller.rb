2ef0ff87be45edc004e60847463ec987fb3d743d
takehisatsugimatsu <cisongwujiu@yahoo.co.jp>
2016-06-29 21:58:47 +0000

    8.7

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,36 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
+  
+  
+  
+  
+  
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
+  private
+  
+  def set_message
+    @message = Message.find(params[:id])
+  end
+  
+  
+  
   def index
     @message = Message.new
     # Messageを全て取得する
@@ -18,6 +50,7 @@ class MessagesController < ApplicationController
   end
 
   private
+  
   def message_params
     params.require(:message).permit(:name, :body)
   end
